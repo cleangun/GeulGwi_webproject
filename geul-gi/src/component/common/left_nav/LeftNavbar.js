32ea997b97ec383a51bb31c43caa5288928ee9c9
Geon Ahn <cleangun@naver.com>
2023-05-08 02:14:03 +0900

    AhnGeon Recent Commit

--- geul-gi/src/component/common/left_nav/LeftNavbar.js
+++ geul-gi/src/component/common/left_nav/LeftNavbar.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { withTheme } from 'styled-components';
 
-const NavBar = styled.div`
-    position : sticky;
-    width : 80px;
-    height : 500px;
-    top : 20%;
-    left : 0px;
-    background-color: white;
-    box-shadow : 3px 3px 8px 0px grey;
-    border-top-right-radius: 12px;
-    border-bottom-right-radius: 12px;
-    
-    z-index: 1;
-`
+import userIconPath from 'Img/icon/user.png'
+import messageIconPath from 'Img/icon/message.png'
+import bellIconPath from 'Img/icon/bell.png'
 
 const LeftNavbar = () => {
     return (
-        <NavBar />
+        <NavBar>
+            <ItemContainer>
+                {console.log("userPath = "+userIconPath)}
+                <Item styled={{backgroundImage : `url(${userIconPath})` , backgroundSize : `contain`}}>1</Item>
+                <Item>2</Item>
+                <Item>3</Item>
+            </ItemContainer>
+        </NavBar>
     );
 };
+
+// 왼쪽 네비게이션 전체 틀 ( Main Container )
+const NavBar = styled.div`
+    position : fixed;
+    width : 100px;
+    height : 100vh;
+    top : 0%;
+    left : 0px;
+    background-color: white;
+    box-shadow : 3px 3px 8px 0px grey;
+    z-index: 1;
+`
+
+// Item 정렬을 위한 Container
+const ItemContainer = styled.div`
+    display : flex;
+    width : 100%;
+    height : 90%;
+    padding-top : 10px;
+    margin : 0 auto;
+    flex-direction : column;
+    align-items:center;
+    border : 1px solid black;
+
+    list-style : none;
+`
+// Item을 담음 (ItemContainer보다 하위)
+const Item = styled.div`
+    display : inline-block;
+    width : 500px;
+    height : 500px;
+    margin : 5px;
+
+    background-position : center;
+    z-index : 2
+`
 
 export default LeftNavbar;
--- geul-gi/src/component/main/Home.js
+++ geul-gi/src/component/main/Home.js
@@ -4,34 +4,59 @@ import styled from 'styled-components';
 // component Import!
 import Header from './../common/header/Header';
 import LeftNavbar from './../common/left_nav/LeftNavbar';
+
 // src Import!
 import path from 'Img/back_gradient.jpg';
 
+const Home = () => {
+    const [isLogged, setIsLogged] = useState(true)
+    return (
+        <div style={{height : "1500px"}}>
+            { isLogged?
+            '' : <h1><Navigate to="/user" replace={true}/></h1>
+                
+            }
+            <LeftNavbar />
+            <HeadContainer style={{backgroundImage : `url(${path})`, backgroundPosition : "center" , backgroundRepeat : "no-repeat"}}>
+                <Header />
+                <RecomContainer >
+                    {/* 추천글 타이틀 */}
+                    <span className='Recom_Title'>금일의 추천 글</span>
+                    {/* 추천글 Contents */}
+                    <RecomContentsContainer >
+
+                    </RecomContentsContainer>
+                </RecomContainer>
+            </HeadContainer>
+
+        </div>
+    );
+};
 
 // StyledComponent
 const HeadContainer = styled.div`
     position : relative;
     top : 0%;
     width : 100%;
-    height : 500px;
+    height : 400px;
+    
     /* background-image : url("img/back_gradient.jpg"); */
 `
-
-const Home = () => {
-    const [isLogged, setIsLogged] = useState(true)
-    return (
-        <div style={{height : "1500px"}}>
-            <LeftNavbar />
-            <HeadContainer style={{backgroundImage : `url(${path})`}}>
-                <Header />
-            </HeadContainer>
-            { isLogged?
-            console.log(path) : <h1><Navigate to="/user" replace={true}/></h1>
-                
-            }
-            
-        </div>
-    );
-};
+// 추천 Container들
+const RecomContainer = styled.div`
+    padding-top : 80px;
+    width : 700px;
+    height : 76%;
+    background-color : rgba(255,255,255,0);
+    border : 2px solid white;
+    margin : 0 auto;
+`
+const RecomContentsContainer = styled.div`
+    width : calc(100% - 3px);
+    height : 150px;
+    background-color : rgba(255,255,255,0);
+    border : 2px solid white;
+    border-radius : 12px;
+`
 
 export default Home;
