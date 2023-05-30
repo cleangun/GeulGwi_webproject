ca2841cd45c31f8ba23ea23c49f45371a643e21b
cleangun <cleangun@naver.com>
2023-05-30 14:50:58 +0900

    ahngeon commit

--- geul-gwi/src/component/user/register/Register.js
+++ geul-gwi/src/component/user/register/Register.js
@@ -10,6 +10,8 @@ import 'css/user/Register.css'
 import { MdOutlineArrowBackIosNew } from 'react-icons/md';
 
 const Register = () => {
+    // Axios Address
+    const [AxiosAddress,SetAxiosAddress] = useState("http://172.16.113.198:8080/users/join");
     // State값들
     const [Id, setId] = useState("");
     const [Password, setPassword] = useState("");
@@ -19,12 +21,6 @@ const Register = () => {
     const [Email, setEmail] = useState("");
     const [Age, setAge] = useState("");
     const [Gender, setGender] = useState("");
-
-    // 감성, 위로, 동기부여, 시, 소설, 깨달음, 현실직시, 자연, 명언, 사랑, 
-    const [Emotion, setEmotion] = useState(false);     // 감성
-    const [Consolation, setConsolation] = useState(false);  // 위로
-    const [Motivate, setMotivate] = useState(false);
-
     
     // onChangeHandler
     const onIdHandler = (event) => {
@@ -45,8 +41,9 @@ const Register = () => {
     const onAgeHandler = (event) => {
         setAge(event.currentTarget.value);
     }
-    const onGenderHandler = (event) => {
-        setGender(event.currentTarget.value);
+    const onGenderHandler = () => {
+        let genderSelect = document.getElementById('gender_input');
+        setGender(genderSelect.options[genderSelect.selectedIndex].value);
     }
     const onEmailHandler = (event) => {
         setEmail(event.currentTarget.value);
@@ -67,8 +64,8 @@ const Register = () => {
             userGender : Gender,
             userEmail : Email
         }
-        console.log("data : "+data.userId);
-        axios.post("http://172.16.113.198:8080/users/join",data)
+        console.log("data : "+data.userGender);
+        axios.post(AxiosAddress, data)
             .then((response) => {
                 console.log("response => ");
                 console.log(response);
@@ -80,46 +77,82 @@ const Register = () => {
             });
     }
     
-    // Next Button Click
-    const [ShowBox, setShowBox] = useState(true) // true => leftbox , false => rightbox
+    // 감성, 위로, 동기부여, 시, 소설, 깨달음, 현실직시, 자연, 명언, 사랑, 
+    const [Emotion, setEmotion] = useState(false);     // 감성
+    const [Consolation, setConsolation] = useState(false);  // 위로
+    const [Motivate, setMotivate] = useState(false);
+
+    
+    // 다음버튼 Click
+    // const [ShowBox, setShowBox] = useState(true) // true => leftbox , false => rightbox
+    const [Box1_showHide, setBox1Show] = useState("box1Show");
+    const [Box2_showHide, setBox2Show] = useState("box2Hide");
     const ToggleMove = () => {
-        console.log("ShowBox : " + ShowBox);
-        setShowBox(!ShowBox);
+        // console.log("ShowBox : " + ShowBox);
+        // setShowBox(!ShowBox);
+
+        console.log('Box1 : '+ Box1_showHide);
+        console.log('Box2 : '+ Box2_showHide);
+        if (Box1_showHide == 'box1Show'){
+            console.log('Box2 show turn')
+            setBox1Show('box1Hide');
+            setBox2Show('box2Show');
+        }
+        else{
+            console.log('Box1 show turn')
+            setBox1Show('box1Show');
+            setBox2Show('box2Hide');
+        }
     }
+
+    // 아이디 유효성 검사
+    const CheckIdExist = (event) => {
+        axios.post(AxiosAddress,userId)
+        .then((response) => {
+            console.log("CheckId Axios Success");
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+
 
 
     return (
         <RegiContainer>
-            <BackButton onClick={ToggleMove}><MdOutlineArrowBackIosNew size={'20px'}/></BackButton>
-            <PrivacyContainer>
+            <PrivacyContainer className={Box1_showHide}>
                 <form style={{display: 'flex' , flexDirection : 'column', width  : '400px', height : '100%', margin : '0 auto'}} onSubmit={onSubmitHandler}>
                     <h3 className='title'>회원가입</h3>
                     <h5 className='sub_title'>회원정보를 입력해주세요</h5>
                     <label>아이디</label>
-                    <input type='text' value={Id} onChange={onIdHandler}/>
+                    <input type='text' value={Id} onChange={onIdHandler} onMouseOut={CheckIdExist} placeholder='아이디를 입력'/>
                     <label>비밀번호</label>
-                    <input type='password' value={Password} onChange={onPasswordHandler}/>
+                    <input type='password' value={Password} onChange={onPasswordHandler} placeholder='비밀번호 입력'/>
                     <label>비밀번호 확인</label>
-                    <input type='password' value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
+                    <input type='password' value={ConfirmPassword} onChange={onConfirmPasswordHandler} placeholder='비밀번호 확인'/>
                     <label>닉네임</label>
-                    <input type='text' value={NickName} onChange={onNickNameHandler}/>
+                    <input type='text' value={NickName} onChange={onNickNameHandler} placeholder='닉네임 입력'/>
                     <label>이름</label>
-                    <input type='text' value={Name} onChange={onNameHandler}/>
+                    <input type='text' value={Name} onChange={onNameHandler} placeholder='이름 입력'/>
                     <label>이메일</label>
-                    <input type='email' value={Email} onChange={onEmailHandler}/>
+                    <input type='email' value={Email} onChange={onEmailHandler} placeholder='이메일 입력'/>
                     <label>나이</label>
-                    <input type='number' value={Age} onChange={onAgeHandler}/>
+                    <input type='number' value={Age} onChange={onAgeHandler} placeholder='나이 입력'/>
                     <label>성별</label>
-                    <input type='select' value={Gender} onChange={onGenderHandler}/>
+                    <select id='gender_input' onChange={onGenderHandler}>
+                        <option value={'Male'}>남성</option>
+                        <option value={'FeMale'}>여성</option>
+                    </select>
 
-                    <label>태그</label>
                     <NextButton onClick={ToggleMove}>
                         다음
                     </NextButton>
 
                 </form>
             </PrivacyContainer>
-            <TagSelectContainer>
+            <TagSelectContainer className={Box2_showHide}>
+                <BackButton onClick={ToggleMove}><MdOutlineArrowBackIosNew size={'20px'}/></BackButton>
                 <form style={{display: 'flex' , flexDirection : 
                 'column', width  : '400px', height : '100%', margin : '0 auto'}} onSubmit={onSubmitHandler}>
                     <h3 className='title'>태그 선택</h3>
@@ -158,7 +191,7 @@ const RegiContainer = styled.div`
     height : auto;
     min-height : 870px;
     justify-content: center;
-    /* overflow : hidden; */
+    overflow : hidden;
     margin : 0 auto;
     margin-top : calc((100vh - 870px)/2);
 
@@ -168,15 +201,16 @@ const RegiContainer = styled.div`
 const PrivacyContainer = styled.div`
     position : absolute;
     top : 5%;
-    left : ${ShowBox => ShowBox ? '100%' : '-100%'};
+    /* left : ${ShowBox => ShowBox ? '0%' : '-100%'}; */
+    /* left : 0%; */
     width : 100%;
     height : auto;
     min-height : 70%;
     
 `
 const TagSelectContainer = styled(PrivacyContainer)`
-    background-color : pink;
-    left : ${ShowBox => ShowBox ? '100%' : '0%'};
+    /* left : ${ShowBox => ShowBox ? '100%' : '0%'}; */
+    /* left : 100%; */
 `
 const NextButton = styled.button`
     width : 250px;
@@ -196,7 +230,7 @@ const SubmitButton = styled(NextButton)`
 `
 const BackButton = styled.button`
     position : absolute;
-    top : 20px;
+    top : 0px;
     left : 20px;
     display : flex;
     width : 50px;
